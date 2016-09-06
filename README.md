e60985d93f529f70ceb17894a1a0af43196b108e
eknoes <me@eknoes.de>
2016-09-06 09:09:02 +0200

    Implemented QR Scanner

--- README.md
+++ README.md
@@ -1,69 +1,20 @@
-# Hello World PhoneGap Template [![bitHound Score][bithound-img]][bithound-url]
+# Digital Vernisage
+Cordova / Phonegap App for a digital vernisage. Scan a QR Code and get the related information. 
+This is currently an alpha version.
 
-A PhoneGap Hello World template
+## Sample QR Codes
+Take care of the world, you have only this one!
 
-## Usage
+![http://weedocare.eknoes.de/#water](resources/water.gif)
 
-#### PhoneGap CLI
+Being a Zenmaster or being Shakespeare?
 
-The hello-world template is the default when you create a new application using the [phonegap-cli][phonegap-cli-url].
+![http://weedocare.eknoes.de/#zenmaster](resources/zenmaster.gif)
 
-    phonegap create my-app
+Impressive, this example of how much water is used for one glass of wine, isn't it?
 
-Create an app using this template specifically:
+![http://weedocare.eknoes.de/#winevswater](resources/winevswater.gif)
 
-    phonegap create my-app --template hello-world
+Sometimes, both opinions have their point.
 
-To see a list of other available PhoneGap templates:
-
-    phonegap template list
-
-## [config.xml][config-xml]
-
-#### android-minSdkVersion (Android only)
-
-Minimum SDK version supported on the target device. Maximum version is blank by default.
-
-This template sets the minimum to `14`.
-
-    <preference name="android-minSdkVersion" value="14" />
-
-#### &lt;access ...&gt; (All)
-
-This template defaults to wide open access.
-
-    <access origin="*" />
-
-It is strongly encouraged that you restrict access to external resources in your application before releasing to production.
-
-For more information on whitelist configuration, see the [Cordova Whitelist Guide][cordova-whitelist-guide] and the [Cordova Whitelist Plugin documentation][cordova-plugin-whitelist]
-
-## [www/index.html][index-html]
-
-#### Content Security Policy (CSP)
-
-The default CSP is similarly open:
-
-    <meta http-equiv="Content-Security-Policy" content="default-src * 'unsafe-inline'; style-src 'self' 'unsafe-inline'; media-src *" />
-
-Much like the access tag above, you are strongly encouraged to use a more restrictive CSP in production.
-
-A good starting point declaration might be:
-
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self' data: gap: 'unsafe-inline' https://ssl.gstatic.com; style-src 'self' 'unsafe-inline'; media-src *" />
-
-For more information on the Content Security Policy, see the [section on CSP in the Cordova Whitelist Plugin documentation][cordova-plugin-whitelist-csp].
-
-Another good resource for generating a good CSP declaration is [CSP is Awesome][csp-is-awesome]
-
-
-[phonegap-cli-url]: http://github.com/phonegap/phonegap-cli
-[cordova-app]: http://github.com/apache/cordova-app-hello-world
-[bithound-img]: https://www.bithound.io/github/phonegap/phonegap-app-hello-world/badges/score.svg
-[bithound-url]: https://www.bithound.io/github/phonegap/phonegap-app-hello-world
-[config-xml]: https://github.com/phonegap/phonegap-template-hello-world/blob/master/config.xml
-[index-html]: https://github.com/phonegap/phonegap-template-hello-world/blob/master/www/index.html
-[cordova-whitelist-guide]: https://cordova.apache.org/docs/en/dev/guide/appdev/whitelist/index.html
-[cordova-plugin-whitelist]: http://cordova.apache.org/docs/en/latest/reference/cordova-plugin-whitelist
-[cordova-plugin-whitelist-csp]: http://cordova.apache.org/docs/en/latest/reference/cordova-plugin-whitelist#content-security-policy
-[csp-is-awesome]: http://cspisawesome.com
+![http://weedocare.eknoes.de/#youhaveapoint](resources/youhaveapoint.gif)
--- www/js/index.js
+++ www/js/index.js
@@ -17,34 +17,93 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
-        downloadFile();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+
+    onDeviceReady: function () {
         app.receivedEvent('deviceready');
+        console.log("Device Ready");
+        app.loadGalleryInfo();
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
+
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        if (parentElement != null) {
+            var listeningElement = parentElement.querySelector('.listening');
+            var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+        }
         console.log('Received Event: ' + id);
+    },
+
+    images: [],
+    baseUrl: null,
+
+    /*
+        Download & Evaluation of JSON Information
+     */
+    loadGalleryInfo: function () {
+        console.log("Loading gallery information");
+        $.getJSON('http://weedocare.eknoes.de/blog.php?callback=?')
+            .done(function (blog) {
+                var i,
+                    current,
+                    imgId;
+                console.log("Got " + blog.blogentries.length + " entries");
+                for (i = 0; i < blog.blogentries.length; i = i + 1) {
+                    current = blog.blogentries[i];
+                    imgId = current.url.split("#")[1];
+                    app.images[imgId] = current;
+                    console.log("Got img:" + imgId);
+                }
+                app.baseUrl = blog.baseUrl;
+            })
+            .fail(function (error) {
+                console.log("Got error");
+                console.log(error);
+            });
+    },
+
+    /*
+    Initializing scan of QR Code
+     */
+    initScan: function () {
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                console.log(result);
+                console.log(app.images[result.text]);
+                app.showInfo(result.text.split("#")[1])
+            },
+            function (error) {
+                alert("Scanning failed: " + error);
+            },
+            {
+                "preferFrontCamera": false, // iOS and Android
+                "showFlipCameraButton": true, // iOS and Android
+                "prompt": "Place a barcode inside the scan area", // supported on Android only
+                "formats": "QR_CODE,PDF_417" // default: all but PDF_417 and RSS_EXPANDED
+            }
+        );
+    },
+
+    /*
+    Show Information of QR Code
+     */
+    showInfo: function (id) {
+        console.log("Show Object " + id);
+        var currentObj = app.images[id];
+        $("#imgTitle").html(currentObj.title);
+        $("#image").attr("src", app.baseUrl + "/" + currentObj.source);
     }
+};
+
+window.onload = function () {
+    app.initialize();
 };
