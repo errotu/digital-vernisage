0a80b05be5d25d5e4e43d6d8b0923ce655df1bc7
eknoes <me@eknoes.de>
2016-09-27 19:52:07 +0200

    Improved stuff

--- src/components/QRButton.js
+++ src/components/QRButton.js
@@ -6,30 +6,34 @@ import {ERR_INVALID_QR} from "../index";
 export default class QRButton extends React.Component {
 
     render() {
-        return (<Fab style={{backgroundColor: ons.platform.isIOS() ? '#4282cc' : null}} onClick={this.handleClick.bind(this)}
-                                    position='bottom right'>
-                <Icon icon='fa-qrcode' size={26} fixedWidth={false} style={{verticalAlign: 'middle'}} />
-            </Fab>);
+        return (<Fab style={{backgroundColor: ons.platform.isIOS() ? '#4282cc' : null}}
+                     onClick={this.handleClick.bind(this)}
+                     position='bottom right'>
+            <Icon icon='fa-qrcode' size={26} fixedWidth={false} style={{verticalAlign: 'middle'}}/>
+        </Fab>);
     }
 
     handleClick() {
-        cordova.plugins.barcodeScanner.scan(
-            (result) => {
-                this.props.navigation.pushPage("qr-code", result.text);
-            },
-            (error) => {
-                console.log(error);
-                if (error != "No barcode") {
-                    this.props.navigation.displayError(ERR_INVALID_QR);
+        if (cordova === undefined) {
+            ons.notification.alert('Cordova is not available in `npm start`');
+        } else {
+            cordova.plugins.barcodeScanner.scan(
+                (result) => {
+                    this.props.navigation.pushPage("qr-code", result.text);
+                },
+                (error) => {
+                    console.log(error);
+                    if (error != "No barcode") {
+                        this.props.navigation.displayError(ERR_INVALID_QR);
+                    }
+                },
+                {
+                    "preferFrontCamera": false, // iOS and Android
+                    "showFlipCameraButton": true, // iOS and Android
+                    "prompt": "Place a barcode inside the scan area", // supported on Android only
+                    "formats": "QR_CODE,PDF_417" // default: all but PDF_417 and RSS_EXPANDED
                 }
-            },
-            {
-                "preferFrontCamera": false, // iOS and Android
-                "showFlipCameraButton": true, // iOS and Android
-                "prompt": "Place a barcode inside the scan area", // supported on Android only
-                "formats": "QR_CODE,PDF_417" // default: all but PDF_417 and RSS_EXPANDED
-            }
-        );
+            );
+        }
     }
-    /* TODO: Handle Barcode scan */
 }
