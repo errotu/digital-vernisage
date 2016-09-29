18ade159d8b5e624fea5fbf0bdb4c4055b3b915f
eknoes <me@eknoes.de>
2016-09-29 07:00:39 +0200

    Changed Information Icon

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,21 +1,19 @@
-import React from 'react';
-
-import {
-    Toolbar as OnsToolbar,
-    BackButton,
-    Icon
-} from 'react-onsenui'
+import React from "react";
+import {Toolbar as OnsToolbar, BackButton, Icon} from "react-onsenui";
 
 export default class Toolbar extends React.Component {
 
     render() {
-        const backButton = this.props.backButton === true
-            ? <BackButton onClick={this.props.navigation.popPage}>Back</BackButton>
+        const backButton = this.props.backButton === true ?
+            <BackButton onClick={this.props.navigation.popPage}>Back</BackButton>
             : null;
+        const infoButton = this.props.view.page == "contact" ?
+            null
+            : <Icon icon="fa-info" onClick={this.handleClick.bind(this)}/>;
         return (<OnsToolbar>
             <div className='left'>{backButton}</div>
             <div className="center">Digital Vernisage</div>
-            <div className="right"><Icon icon="fa-envelope" onClick={this.handleClick.bind(this)} /></div>
+            <div className="right">{infoButton}</div>
         </OnsToolbar>);
     }
 
