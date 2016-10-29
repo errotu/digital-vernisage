d521d1403abfa8d67bdf69e46aac2b70bd419607
eknoes <me@eknoes.de>
2016-10-29 18:28:13 +0200

    Can't fix iOS 8

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,17 +5,16 @@ var logo = require('../static/logo.png');
 export default class Contact extends React.Component {
 
     render() {
-        return (<div className="contact-page">
-            <div width="100%">
+        return (<Row className="contact-page">
+            <Col width="100%">
                 <img src={logo} title="WeeDooCare-Logo" alt="WeeDooCare-Logo"/>
-            </div>
-            <div>
+            </Col>
+            <Col width="100%">
                 <p>We see the world as an ecosystem, and combine facts with poetry and art.</p>
                 <p>With this Digital Vernissage we want to show our GreenArt. Unique green cartoons and art galleries to
                     guide people for a mindset change.</p>
 
-                <p>See more on <a href="https://www.weedoocare.com">www.weedoocare.com</a></p>
-            </div>
-        </div>);
+                <p>See more on <a href="https://www.weedoocare.com">www.weedoocare.com</a></p></Col>
+        </Row>);
     }
 }
